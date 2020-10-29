806a310d6a31afeae669aef08820e6a97aed6cd1
kewenjie <1164400308@qq.com>
2020-10-29 23:21:09 +0800

    project init

---
+++ src/main/java/com/wukong/edu/RocketApplication.java
@@ -0,0 +1,17 @@
+package com.wukong.edu;
+
+
+/**
+ * @ Author     ：kwj.
+ * @ Date       ：Created in 20:34 2020/4/27 0027
+ * @ Description：
+ * @ Modified By：
+ * 欢迎访问悟空课堂 http://www.itwukong.com/
+ */
+public class RocketApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
